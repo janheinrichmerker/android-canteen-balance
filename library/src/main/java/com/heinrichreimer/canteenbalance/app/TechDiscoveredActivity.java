6c3f845202c6c69e4792f8231951075b111bf33f
Lena Stepanova <e.stepanova@enveto.de>
2022-01-24 15:30:13 +0100

    update to match android sdk 31

--- library/src/main/java/com/heinrichreimer/canteenbalance/app/TechDiscoveredActivity.java
+++ library/src/main/java/com/heinrichreimer/canteenbalance/app/TechDiscoveredActivity.java
@@ -24,7 +24,7 @@ import android.content.Intent;
 import android.nfc.NfcAdapter;
 import android.nfc.Tag;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.util.Log;
 
 import com.heinrichreimer.canteenbalance.cardreader.CardBalance;
